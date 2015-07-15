464d11f88f588b96f22cefbf0a451bc43e21243c
AlexeiSleptcov <alexei.sleptcov@medgenetics.ru>
2015-07-15 16:37:14 +0600

    bug fix

--- R/plotDG.R
+++ R/plotDG.R
@@ -3,21 +3,17 @@
 #' Clustering tree by Hierarchical clustering algorithm.
 #' 
 #' @param data microarray data. Only for \code{RGlist}, \code{MAlist} or \code{ELits} class objects.
-#' @param cyanine only for RGList data class.
-#' 
 #' @param methodDist method for Distance Matrix Computation. See \code{\link{dist}}. Default: {"euclidean"}.
 #' @param methodHclust method for hierarchical cluster analysis. See \code{\link{hclust}}. Default: {"complete"}.
 #'  
 #' @export
 
 plotDG <- function(data, 
-                   cyanine = c("R","G"),
                    methodDist = "euclidean",
                    methodHclust = "complete"){
   
   if(inherits(data,"RGlist")){
-    cyanine = match.arg(cyanine)
-    dannye = data[[cyanine]]
+    dannye = log10(data$R/data$G)
   } else if(inherits(data,"MAlist")) {
     dannye = data$M
   } else if(inherits(data,"Elist")) {
@@ -36,28 +32,22 @@ plotDG <- function(data,
 #' Clustering MDS by K-Means clustering algorithm.
 #' 
 #' @param data microarray data. Only for \code{RGlist}, \code{MAlist} or \code{ELits} class objects.
-#' @param cyanine only for RGList data class.
 #' @param centers either the number of clusters, say \emph{k}, or a set initial (distinct)
 #' cluster centers. If a number, a random set of (distinct) rows in x is chosen as initial centres.
 #' See \code{\link{kmeans}}. Default: \code{2}.
 #' 
 #' @param methodDist method for Distance Matrix Computation. See \code{\link{dist}}. Default: {"euclidean"}.
-#' @param methodHclust method for hierarchical cluster analysis. See \code{\link{hclust}}. Default: {"complete"}.
-#' 
 #' @param print print result K-Means clustering
 #'  
 #' @export
 
-plotDG <- function(data, 
-                   cyanine = c("R","G"),
+plotKM <- function(data, 
                    centers = 2,
                    methodDist = "euclidean",
-                   methodHclust = "complete",
                    print = FALSE){
   
   if(inherits(data,"RGList")){
-    cyanine = match.arg(cyanine)
-    dannye = data[[cyanine]]
+    dannye = log10(data$R/data$G)
   } else if(inherits(data,"MAList")) {
     dannye = data$M
   } else if(inherits(data,"EList")) {
@@ -66,7 +56,7 @@ plotDG <- function(data,
     stop("Data must RGList, MAList or EList class object")
   }
   
-  MDS.data = cmdscale( dist( t(dannye), method = methodDist), method = methodHclust);
+  MDS.data = cmdscale( dist( t(dannye), method = methodDist));
   km = kmeans(MDS.data, centers, nstart=1000)
   plot(MDS.data, type="n")
   text(MDS.data, rownames(MDS.data), col = km$cluster)
