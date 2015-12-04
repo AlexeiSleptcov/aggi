b06a27d9dac0b7f1b7279fadc681c896433207aa
alexeisleptcov <alexei.sleptcov@medgenetics.ru>
2015-12-04 13:11:51 +0600

    fix to only fit

--- R/noiseReductionCGH.R
+++ R/noiseReductionCGH.R
@@ -17,25 +17,25 @@ noiseReductionCGH <- function(data, N = NULL, method = c("mad", "iqr")){
   }
   
   for(i in 1:ncol(data$R)){
-    Rf2 = data$R[,i]
-    Gf2 = data$G[,i]
-    Rm = median(Rf2, na.rm = TRUE)
-    Gm = median(Gf2, na.rm = TRUE)
-    if(match.arg(method) == "mad") {
-      Rmad = mad(Rf2, na.rm = TRUE)/2
-      Gmad = mad(Gf2, na.rm = TRUE)/2
-    } else {
-      Rmad = IQR(Rf2, na.rm = TRUE)/2
-      Gmad = IQR(Gf2, na.rm = TRUE)/2
-    }
-    if(is.null(N))
-      N = mean(c(Rmad, Gmad))
-    
-    # AFFINE TRANSFORMATION
-    # SUBTRACT MEDIAN BEFORE & ADD AFTER
-    # FITTING MAD TO COMMON MAD
-    data$R[,i] = ( ( (Rf2-Rm)*(N/Rmad) )+Rm )
-    data$G[,i] = ( ( (Gf2-Gm)*(N/Gmad) )+Gm )
+#     Rf2 = data$R[,i]
+#     Gf2 = data$G[,i]
+#     Rm = median(Rf2, na.rm = TRUE)
+#     Gm = median(Gf2, na.rm = TRUE)
+#     if(match.arg(method) == "mad") {
+#       Rmad = mad(Rf2, na.rm = TRUE)/2
+#       Gmad = mad(Gf2, na.rm = TRUE)/2
+#     } else {
+#       Rmad = IQR(Rf2, na.rm = TRUE)/2
+#       Gmad = IQR(Gf2, na.rm = TRUE)/2
+#     }
+#     if(is.null(N))
+#       N = mean(c(Rmad, Gmad))
+#     
+#     # AFFINE TRANSFORMATION
+#     # SUBTRACT MEDIAN BEFORE & ADD AFTER
+#     # FITTING MAD TO COMMON MAD
+#     data$R[,i] = ( ( (Rf2-Rm)*(N/Rmad) )+Rm )
+#     data$G[,i] = ( ( (Gf2-Gm)*(N/Gmad) )+Gm )
     
 #     if(inherits(data, "RGlist")){
 #       data$R[,i] = data$R[,i]-median(data$R[data$genes$SubTypeMask == 1028,i], na.rm = TRUE)
